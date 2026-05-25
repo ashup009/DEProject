4a83327d794a88b53a5d382911e41c2dc95fc6d3
Ashutosh Prakash <ashutoshp2@icloud.com>
2026-05-25 14:35:13 +0530

    CI test

--- src/deproject/main.py
+++ src/deproject/main.py
@@ -1,2 +1,6 @@
 def add(a, b):
     return a + b
+
+
+def mul(a: int, b: int):
+    return a * b
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,12 @@
-from deproject.main import add
+from deproject.main import add, mul
 
 
 def test_add():
     assert add(1, 2) == 3
     assert add(-1, 1) == 0
     assert add(-1, -1) == -2
+
+def test_mul():
+    assert mul(1, 2) == 2
+    assert mul(-1, 1) == -1
+    assert mul(-1, -1) == 1
